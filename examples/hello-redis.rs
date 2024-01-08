cff20cf743587b15aca87112d18403c1bc145c25
Wang Yunfeng <wangyunfeng@chinamoney.com.cn>
2024-01-08 10:46:31 +0800

    create async tasks

---
+++ examples/hello-redis.rs
@@ -0,0 +1,17 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // 连接到 Redis 服务器
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // 设置键值对
+    let res = client.set("hello", "world".into()).await?;
+    println!("{:?}", res);
+
+    // 获取
+    let result = client.get("hello").await?;
+
+    println!("{:?}", result.unwrap());
+    Ok(())
+}
